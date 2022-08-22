3587c695100b43fd0ad33d6e53f3dd7ca0ab85bf
AlexGavalas <alex.gavalas.92@gmail.com>
2022-08-22 15:47:26 +0300

    feat: added list with flip animation

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
+    import { fade } from 'svelte/transition';
+    import { flip } from 'svelte/animate';
     import Modal from '$lib/components/Modal.svelte';
 
     let showModal = false;
+    let items: { name: string; key: string }[] = [];
 
     const toggleModal = () => {
         showModal = !showModal;
+    };
+
+    const addItem = () => {
+        items = [
+            {
+                name: `Item #${items.length + 1}`,
+                // Key should be unique, Math.random is used for demo purposes
+                key: (Math.random() * 10000).toString(),
+            },
+            ...items,
+        ];
     };
 </script>
 
@@ -13,3 +27,15 @@
 </Modal>
 
 <button on:click={toggleModal}>Open modal</button>
+<button on:click={addItem}>Add an item</button>
+
+<div>
+    {#each items as item (item.key)}
+        <div in:fade animate:flip={{ duration: 500 }}>
+            <p>{item.name}</p>
+            <button on:click={() => (items = items.filter((v) => v !== item))}> Delete item</button>
+        </div>
+    {:else}
+        <p>No items</p>
+    {/each}
+</div>
