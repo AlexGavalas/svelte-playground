2ade1bea0e9d6bd0df1f3a5686a553fe87d45920
AlexGavalas <alex.gavalas.92@gmail.com>
2022-08-24 22:11:47 +0300

    feat: prefetch links on hover

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,16 +3,16 @@
     <nav>
         <ul>
             <li>
-                <a href="/blog">Blog</a>
+                <a sveltekit:prefetch href="/blog">Blog</a>
             </li>
             <li>
-                <a href="/poker">Poker</a>
+                <a sveltekit:prefetch href="/poker">Poker</a>
             </li>
             <li>
-                <a href="/todos">Todos</a>
+                <a sveltekit:prefetch href="/todos">Todos</a>
             </li>
             <li>
-                <a href="/about">About</a>
+                <a sveltekit:prefetch href="/about">About</a>
             </li>
         </ul>
     </nav>
